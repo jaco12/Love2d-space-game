b958f1360ec53c1beeb56631527daf5184d61ac1
Quetheria <78924156+Quetheria@users.noreply.github.com>
2021-02-16 10:05:43 -0500

    Update 2/16 Space game

--- main.lua
+++ main.lua
@@ -1,21 +1,25 @@
 --Imports
-rick=love.graphics.newImage("rickImage.png")
-tile=love.graphics.newImage("tile.png")
-mainTheme = love.audio.newSource("URMUM.mp3", "static")
-int=2
+function love.load()
+	rick=love.graphics.newImage("rickImage.png")
+	tile=love.graphics.newImage("tile.png")
+	mainTheme = love.audio.newSource("URMUM.mp3", "static")
+	int=2
 --vars
-x=0
-y=0
-speed = 50
---original set up
-love.audio.play(mainTheme)
+	x=0
+	y=0
+	speed = 80
+	syncopateRaster = love.font.newRasterizer("8BIT WONDER Nominal.ttf")
+	syncopate = love.font.newFontData(syncopateRaster)
+	playable=false
+end
+
 --every frame
 function love.update(dt)
-
+	love.audio.play(mainTheme)
 			if love.keyboard.isDown('right') then
-	      x = x + (speed * dt)
+	      x = x - (speed * dt)
 			elseif love.keyboard.isDown('left') then
-				x = x - (speed * dt)
+				x = x + (speed * dt)
 			elseif love.keyboard.isDown('down') then
 				y=y-(speed*dt)
 			elseif love.keyboard.isDown('up') then
@@ -24,5 +28,11 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.draw(tile, x, y)
+	if playable == true then
+		love.graphics.draw(tile, x, y)
+		love.graphics.draw(tile, x+100, y+100)
+		love.graphics.draw(tile, x-100, y-100)
+		love.graphics.draw(tile, x+100, y)
+		love.graphics.draw(tile, x, y+100)
+	end
 end
